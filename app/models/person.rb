bde9185845f6cc815e819930e2084b939f20b4a2
Ed Lenox <edlenox@gmail.com>
2016-03-08 16:13:53 +0000

    added age to json

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,18 @@
 class Person < ActiveRecord::Base
 
   validates :first_name, :last_name, :birth_date, presence: true
+
+  def age
+    return nil unless birth_date
+    now = Date.current
+    years_since_birth = now.year - birth_date.year
+    years_since_birth -= 1 if now.yday < birth_date.yday
+    years_since_birth
+  end
+
+
+  def as_json(options={})
+    {id: id, first_name: first_name, last_name: last_name, age: age}
+  end
+
 end
